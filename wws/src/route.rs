3329e356a4cb8647827e45dde3070582c50316a3
Emmie Maeda <emmie.maeda@gmail.com>
2025-01-16 02:22:09 -0500

    Use fallback for routing.
    Finally, this is how you do the catch-all.

--- wws/src/route.rs
+++ wws/src/route.rs
@@ -62,7 +62,7 @@ pub fn build_router(state: ServerState) -> Router {
         .route("/-/download/{*rest}", any(redirect_to_files))
         .route("/-/code/{*rest}", any(redirect_to_files))
         .route("/-/html/{*rest}", any(redirect_to_files))
-        .route("/", any(proxy_framerail))
+        .fallback(proxy_framerail)
         .with_state(main_state);
 
     // Router that serves wjfiles
@@ -81,111 +81,108 @@ pub fn build_router(state: ServerState) -> Router {
         )
         .route("/-/code/{page_slug}/{index}", get(handle_code_block))
         .route("/-/html/{page_slug}/{hash}", get(handle_html_block))
-        .route("/", get(handle_hello_world))
+        .fallback(handle_hello_world)
         .with_state(file_state);
 
     Router::new()
         // Domain delegation logic
-        .route(
-            "/",
-            any(
-                |State(state): State<ServerState>,
-                 Host(hostname): Host,
-                 mut request: Request<Body>| async move {
-                    {
-                        let headers = request.headers_mut();
+        .fallback(
+            |State(state): State<ServerState>,
+             Host(hostname): Host,
+             mut request: Request<Body>| async move {
+                 {
+                     let headers = request.headers_mut();
 
-                        // Strip internal headers, just to be safe.
-                        headers.remove("x-wikijump-site-id");
-                        headers.remove("x-wikijump-site-slug");
-                        headers.remove("x-wikijump-domain");
+                     // Strip internal headers, just to be safe.
+                     headers.remove("x-wikijump-site-id");
+                     headers.remove("x-wikijump-site-slug");
+                     headers.remove("x-wikijump-domain");
 
-                        // Also add the domain header since that is the same before lookup_host()
-                        headers.insert("x-wikijump-domain", header_value!(hostname));
-                    }
+                     // Also add the domain header since that is the same before lookup_host()
+                     headers.insert("x-wikijump-domain", header_value!(hostname));
+                 }
 
-                    macro_rules! forward_request {
-                        ($router:expr) => {
-                            match $router.oneshot(request).await {
-                                Ok(response) => response,
-                                Err(infallible) => match infallible {},
-                            }
-                        };
-                    }
+                 macro_rules! forward_request {
+                     ($router:expr) => {
+                         match $router.oneshot(request).await {
+                             Ok(response) => response,
+                             Err(infallible) => match infallible {},
+                         }
+                     };
+                 }
 
-                    macro_rules! add_headers {
-                        ($site_id:expr, $site_slug:expr) => {{
-                            // Validate types
-                            let _: i64 = $site_id;
-                            let _: &str = &$site_slug;
+                 macro_rules! add_headers {
+                     ($site_id:expr, $site_slug:expr) => {{
+                         // Validate types
+                         let _: i64 = $site_id;
+                         let _: &str = &$site_slug;
 
-                            // Add headers
-                            let headers = request.headers_mut();
-                            headers.insert("x-wikijump-site-id", header_value!(str!($site_id)));
-                            headers.insert("x-wikijump-site-slug", header_value!($site_slug));
-                        }};
-                    }
+                         // Add headers
+                         let headers = request.headers_mut();
+                         headers.insert("x-wikijump-site-id", header_value!(str!($site_id)));
+                         headers.insert("x-wikijump-site-slug", header_value!($site_slug));
+                     }};
+                 }
 
-                    // Determine what host and site (e.g. main vs files, what site slug and ID)
-                    let host_data = match lookup_host(&state, &hostname).await {
-                        Ok(host_data) => host_data,
-                        Err(error) => {
-                            // TODO error page response in case of an internal issue
-                            todo!()
-                        }
-                    };
+                 // Determine what host and site (e.g. main vs files, what site slug and ID)
+                 let host_data = match lookup_host(&state, &hostname).await {
+                     Ok(host_data) => host_data,
+                     Err(error) => {
+                         // TODO error page response in case of an internal issue
+                         todo!()
+                     }
+                 };
 
-                    // Now that we have the general category of request type, we can
-                    // give it to the right place to be processed.
-                    match host_data {
-                        // Main site route handling
-                        SiteAndHost::Main { site_id, site_slug } => {
-                            add_headers!(site_id, site_slug);
-                            forward_request!(main_router)
-                        }
-                        SiteAndHost::MainCustom { site_id, site_slug } => {
-                            // NOTE: The difference here is site_slug here is String not &str
-                            add_headers!(site_id, site_slug);
-                            forward_request!(main_router)
-                        }
-                        // Main site missing
-                        SiteAndHost::MainMissing { site_slug } => {
-                            // TODO
-                            forward_request!(main_router)
-                        }
-                        SiteAndHost::MainCustomMissing => {
-                            todo!()
-                        }
-                        // Default site redirect
-                        // e.g. "www.wikijump.com/foo" -> "wikijump.com/foo"
-                        SiteAndHost::DefaultRedirect => {
-                            let destination = format!(
-                                "https://{}{}",
-                                state.domains.main_domain_no_dot,
-                                get_path(request.uri()),
-                            );
-                            Redirect::permanent(&destination).into_response()
-                        }
-                        // Files site route handling
-                        SiteAndHost::File { site_id, site_slug } => {
-                            add_headers!(site_id, site_slug);
-                            forward_request!(files_router)
-                        }
-                        SiteAndHost::FileMissing { site_slug } => {
-                            // TODO
-                            forward_request!(files_router)
-                        }
-                        // Files site by itself
-                        // See the case in host.rs for an explanation
-                        SiteAndHost::FileRoot => {
-                            let destination =
-                                format!("https://{}", state.domains.main_domain_no_dot);
+                 // Now that we have the general category of request type, we can
+                 // give it to the right place to be processed.
+                 match host_data {
+                     // Main site route handling
+                     SiteAndHost::Main { site_id, site_slug } => {
+                         add_headers!(site_id, site_slug);
+                         forward_request!(main_router)
+                     }
+                     SiteAndHost::MainCustom { site_id, site_slug } => {
+                         // NOTE: The difference here is site_slug here is String not &str
+                         add_headers!(site_id, site_slug);
+                         forward_request!(main_router)
+                     }
+                     // Main site missing
+                     SiteAndHost::MainMissing { site_slug } => {
+                         // TODO
+                         forward_request!(main_router)
+                     }
+                     SiteAndHost::MainCustomMissing => {
+                         todo!()
+                     }
+                     // Default site redirect
+                     // e.g. "www.wikijump.com/foo" -> "wikijump.com/foo"
+                     SiteAndHost::DefaultRedirect => {
+                         let destination = format!(
+                             "https://{}{}",
+                             state.domains.main_domain_no_dot,
+                             get_path(request.uri()),
+                         );
+                         Redirect::permanent(&destination).into_response()
+                     }
+                     // Files site route handling
+                     SiteAndHost::File { site_id, site_slug } => {
+                         add_headers!(site_id, site_slug);
+                         forward_request!(files_router)
+                     }
+                     SiteAndHost::FileMissing { site_slug } => {
+                         // TODO
+                         forward_request!(files_router)
+                     }
+                     // Files site by itself
+                     // See the case in host.rs for an explanation
+                     SiteAndHost::FileRoot => {
+                         let destination =
+                             format!("https://{}", state.domains.main_domain_no_dot);
 
-                            Redirect::temporary(&destination).into_response()
-                        }
-                    }
-                },
-            ),
+                         Redirect::temporary(&destination).into_response()
+                     }
+                 }
+             }
         )
         // Easter egg
         .route("/-/teapot", any(handle_teapot))
