da4aa487270da61c741061f353a7fb70b83f8551
Emmie Maeda <emmie.maeda@gmail.com>
2025-04-22 01:01:40 -0400

    Revert incorrect relation changes.
    This is a bug in sea-orm-cli that is causing it to generate a relation
    for preferred_site_domain but neglect the relation to site_id, which
    is what we actually want. It prevented us from being able to query
    the associated site of a custom domain.

--- deepwell/src/models/site.rs
+++ deepwell/src/models/site.rs
@@ -51,6 +51,8 @@ pub enum Relation {
     PageConnectionMissing,
     #[sea_orm(has_many = "super::page_revision::Entity")]
     PageRevision,
+    #[sea_orm(has_many = "super::site_domain::Entity")]
+    SiteDomain,
     #[sea_orm(
         belongs_to = "super::site_domain::Entity",
         from = "Column::PreferredDomain",
@@ -58,7 +60,7 @@ pub enum Relation {
         on_update = "NoAction",
         on_delete = "NoAction"
     )]
-    SiteDomain,
+    SiteDomainPreferredDomain,
 }
 
 impl Related<super::file::Entity> for Entity {
