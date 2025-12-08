796b465ecde00bf88b5ba689276fb10c823fd060
Syxles <syxles.contact@gmail.com>
2025-12-09 00:01:02 +0100

    Update health-check.sh

--- install/local/postgres/health-check.sh
+++ install/local/postgres/health-check.sh
@@ -1,7 +1,1 @@
-#!/bin/sh
-
-# Database is running
-sudo -u wikijump pg_isready -d wikijump
-
-# Database is accessible via network
-sudo -u wikijump pg_isready -d wikijump -h localhost -p 5432
+../../common/postgres/health-check.sh
