7047468461019fd6aba8f86903f97ec5dbb04a8b
Emmie Maeda <emmie.maeda@gmail.com>
2026-01-05 01:20:55 -0500

    Add translate_strings integration test.

--- deepwell/tests/locale.rs
+++ deepwell/tests/locale.rs
@@ -35,13 +35,145 @@ async fn locale_info() {
     assert_eq!(info.script, None);
     assert!(info.variants.is_empty());
 
-    let info = run_endpoint!(endpoints::locale::locale_info, ctx, r#"["fr_Latn-FR-MACOS"]"#);
+    let info = run_endpoint!(
+        endpoints::locale::locale_info,
+        ctx,
+        r#"["fr_Latn-FR-MACOS"]"#,
+    );
     assert_eq!(info.language, "fr");
     assert_str_eq!(info.region, Some("FR"));
     assert_str_eq!(info.script, Some("Latn"));
     assert_eq!(info.variants.len(), 1);
     assert_eq!(info.variants[0], "macos");
 
+    cleanup!(state, txn, ctx);
+}
+
+#[tokio::test]
+async fn translate_strings() {
+    let (state, txn) = common::setup().await;
+    let ctx = ServiceContext::new(&state, &txn);
+
+    // Error cases
+
+    let error = run_endpoint_err!(
+        endpoints::locale::translate_strings,
+        ctx,
+        r#"{"locales": [], "messages": {"license": {}, "base-title": {"title": "foo"}}}"#,
+    );
+    assert!(matches!(error, ServiceError::NoLocalesSpecified));
+
+    let error = run_endpoint_err!(
+        endpoints::locale::translate_strings,
+        ctx,
+        r#"{"locales": ["fr_FR"], "messages": {"license": {}}, "strip_message_keys": ["base-title"]}"#,
+    );
+    assert!(matches!(error, ServiceError::BadRequest));
+
+    let error = run_endpoint_err!(
+        endpoints::locale::translate_strings,
+        ctx,
+        r#"{"locales": ["fr_FR"], "messages": {"license": {}, "base-title": {"title": "foo"}}, "strip_message_keys": ["xyz-invalid-key"]}"#,
+    );
+    assert!(matches!(error, ServiceError::BadRequest));
+
+    // Success cases
+
+    let output = run_endpoint!(
+        endpoints::locale::translate_strings,
+        ctx,
+        r#"{"locales": ["en"], "messages": {"license": {}, "license.cc0": {}, "base-title": {"title": "foo"}}}"#,
+    );
+    assert_eq!(output.len(), 3);
+    assert_str_eq!(output["license"], Some("License"));
+    assert_str_eq!(output["license.cc0"], Some("Public Domain (CC0)"));
+    assert_str_eq!(output["base-title"], Some("\u{2068}foo\u{2069} | Wikijump"));
+
+    let output = run_endpoint!(
+        endpoints::locale::translate_strings,
+        ctx,
+        r#"{"locales": ["fr_BE", "fr_FR", "de"], "messages": {"alt-title": {}, "close": {}}}"#,
+    );
+    assert_eq!(output.len(), 2);
+    assert!(output.contains_key("alt-title"));
+    assert!(output.contains_key("close"));
+
+    // Null outputs
+
+    // No locale
+    let output = run_endpoint!(
+        endpoints::locale::translate_strings,
+        ctx,
+        r#"{"locales": ["xyz_US"], "messages": {"license": {}}}"#,
+    );
+    assert_eq!(output.len(), 1);
+    assert!(output["license"].is_none());
+
+    // No message
+    let output = run_endpoint!(
+        endpoints::locale::translate_strings,
+        ctx,
+        r#"{"locales": ["en"], "messages": {"xyz": {}}}"#,
+    );
+    assert_eq!(output.len(), 1);
+    assert!(output["xyz"].is_none());
+
+    // No message value
+    let output = run_endpoint!(
+        endpoints::locale::translate_strings,
+        ctx,
+        r#"{"locales": ["en"], "messages": {"error-404": {}, "error-404.page": {}}}"#,
+    );
+    assert_eq!(output.len(), 2);
+    assert!(output["error-404"].is_none());
+    assert!(output["error-404.page"].is_some());
+
+    // No message attribute
+    let output = run_endpoint!(
+        endpoints::locale::translate_strings,
+        ctx,
+        r#"{"locales": ["en"], "messages": {"license.cc0": {}, "license.xyz": {}}}"#,
+    );
+    assert_eq!(output.len(), 2);
+    assert!(output["license.cc0"].is_some());
+    assert!(output["license.xyz"].is_none());
+
+    // No such
+    let output = run_endpoint!(
+        endpoints::locale::translate_strings,
+        ctx,
+        r#"{"locales": ["en"], "messages": {"license.cc0": {}, "license.xyz": {}}}"#,
+    );
+    assert_eq!(output.len(), 2);
+    assert!(output["license.cc0"].is_some());
+    assert!(output["license.xyz"].is_none());
+
+    // Testing strip_message_keys
+
+    let output = run_endpoint!(
+        endpoints::locale::translate_strings,
+        ctx,
+        r#"{"locales": ["en"], "messages": {"base-title": {"title": "foo"}}, "strip_message_keys": ["base-title"]}"#,
+    );
+    assert_eq!(output.len(), 1);
+    assert_str_eq!(output["base-title"], Some("foo | Wikijump"));
+
+    let output = run_endpoint!(
+        endpoints::locale::translate_strings,
+        ctx,
+        r#"{"locales": ["en"], "messages": {"base-title": {"title": "foo"}}, "strip_message_keys": []}"#,
+    );
+    assert_eq!(output.len(), 1);
+    assert_str_eq!(output["base-title"], Some("\u{2068}foo\u{2069} | Wikijump"));
+
+    let output = run_endpoint!(
+        endpoints::locale::translate_strings,
+        ctx,
+        r#"{"locales": ["en"], "messages": {"license.cc0": {}, "license": {}}, "strip_message_keys": ["license", "license.cc0"]}"#,
+    );
+    assert_eq!(output.len(), 2);
+    assert_str_eq!(output["license"], Some("License"));
+    assert_str_eq!(output["license.cc0"], Some("Public Domain (CC0)"));
 
     cleanup!(state, txn, ctx);
 }
