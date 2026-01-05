6010dd819aec6fb8c86e6a96e3655306f1550d18
Emmie Maeda <emmie.maeda@gmail.com>
2026-01-05 01:03:28 -0500

    Add assert_str_eq! test helper macro.

--- deepwell/tests/common/mod.rs
+++ deepwell/tests/common/mod.rs
@@ -18,9 +18,13 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+//! Common utilities for running DEEPWELL integration tests.
+
 use deepwell::api::{ServerState, build_server_state};
 use deepwell::config::{Config, Secrets};
 use sea_orm::{DatabaseTransaction, TransactionTrait};
+
+// Endpoint invocation
 
 macro_rules! params {
     () => {{
@@ -33,6 +37,8 @@ macro_rules! params {
         Params::new(Some($value))
     }};
 }
+
+// Test setup
 
 pub async fn setup() -> (ServerState, DatabaseTransaction) {
     let secrets = Secrets::load();
@@ -98,5 +104,56 @@ macro_rules! cleanup {
             .expect("Unable to roll back transaction");
 
         mem::drop($state);
+    }};
+}
+
+// Test macros
+
+/// Allows for equality assertions on `Option<String>` without boilerplate.
+///
+/// This avoids the type annoyance that comes with these two types,
+/// for improved readability within test code.
+///
+/// This code is simple, readable, and doens't work:
+/// ```compile_fail
+/// let left: Option<String> = Some(str!("foo"));
+/// assert_eq!(left, Some("foo"));
+/// ```
+///
+/// So instead, we have to unwrap it, which either means a bulky
+/// `.expect()` message or a separate assertion in front of it to
+/// catch cases of `None`. Both are inelegant:
+///
+/// ```
+/// # let left: Option<String> = Some(str!("foo"));
+/// assert_eq!(left.expect("Left is None"), "foo");
+/// ```
+///
+/// ```
+/// # let left: Option<String> = Some(str!("foo"));
+/// assert!(left.is_some());
+/// assert_eq!(left.unwrap(), "foo");
+/// ```
+///
+/// Instead, we can use this macro to compare them, which coerces both
+/// types to `Option<&str>` instead, permitting a more natural comparison:
+/// ```
+/// # let left: Option<String> = Some(str!("foo"));
+/// assert_str_eq!(left, Some("foo"));
+/// ```
+#[allow(unused_macros)]
+macro_rules! assert_str_eq {
+    ($left:expr, $right:expr $(,)?) => {{
+        let left: Option<&str> = match $left {
+            Some(ref s) => Some(s.as_str()),
+            None => None,
+        };
+
+        let right: Option<&str> = match $right {
+            Some(ref s) => Some(s.as_str()),
+            None => None,
+        };
+
+        assert_eq!(left, right);
     }};
 }
