82e74369f1fea97a1356a9965cb24db149fcef8a
Emmie Maeda <emmie.maeda@gmail.com>
2026-02-01 19:43:30 -0500

    Convert MfaService structs to new error type.

--- deepwell/src/services/mfa/structs.rs
+++ deepwell/src/services/mfa/structs.rs
@@ -46,7 +46,17 @@ pub struct RecoveryCodes {
 }
 
 impl RecoveryCodes {
-    pub fn generate(config: &Config) -> OldResult<Self> {
+    pub fn generate(config: &Config) -> Result<Self> {
+        let make_error = || {
+            Error::new(
+                format!(
+                    "failed to generate new MFA recovery codes ({} codes each {} bytes long each)",
+                    config.recovery_code_count, config.recovery_code_length,
+                ),
+                ErrorType::UserMfa,
+            )
+        };
+
         let mut rng = thread_rng();
         assert_is_csprng(&rng);
 
@@ -70,7 +80,7 @@ impl RecoveryCodes {
 
             for code in &recovery_codes {
                 debug!("Hashing recovery code");
-                let hash = PasswordService::new_hash(code)?;
+                let hash = PasswordService::new_hash(code).or_raise(make_error)?;
                 hashes.push(hash);
             }
 
