8d01a10c2b83d01b12e7b431faa00324a3cba61f
Zokhoi <zokhoi@zokhoi.com>
2026-04-19 16:04:00 +0800

    Use preloaded data in site admin view

--- framerail/src/routes/[x+2d]/admin/+page.server.ts
+++ framerail/src/routes/[x+2d]/admin/+page.server.ts
@@ -1,7 +1,7 @@
 import { adminAction, loadAdminPage } from "$lib/server/load/admin"
 
-export async function load({ request, cookies }) {
-  return loadAdminPage(request, cookies)
+export async function load({ request, cookies, parent }) {
+  return loadAdminPage(request, cookies, parent)
 }
 
 export const actions = { default: adminAction }
