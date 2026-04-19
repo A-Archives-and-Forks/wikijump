8f634b9e5789b16f88b56b67cd0fdbe0a391ccf8
Zokhoi <zokhoi@zokhoi.com>
2026-04-19 16:04:44 +0800

    Use preloaded data in logout page

--- framerail/src/routes/[x+2d]/logout/+page.server.ts
+++ framerail/src/routes/[x+2d]/logout/+page.server.ts
@@ -1,7 +1,7 @@
 import { loadLogoutPage, logoutAction } from "$lib/server/load/logout"
 
-export async function load({ request, cookies }) {
-  return loadLogoutPage(request, cookies)
+export async function load({ request, cookies, parent }) {
+  return loadLogoutPage(request, cookies, parent)
 }
 
 export const actions = { logout: logoutAction }
