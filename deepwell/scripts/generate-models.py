cbb2f0cc7c3c901e7775a4d029e5428bd1decfde
Emmie Smith <emmie.maeda@gmail.com>
2026-04-19 13:20:23 -0400

    Add some logging.

--- deepwell/scripts/generate-models.py
+++ deepwell/scripts/generate-models.py
@@ -10,6 +10,7 @@ MODELS_DIRECTORY = "src/models"
 
 
 def chdir_to_crate_root():
+    print("Changing directory to deepwell")
     this_script = os.path.abspath(sys.argv[0])
     crate_root = os.path.dirname(os.path.dirname(this_script))
     os.chdir(crate_root)
@@ -17,10 +18,12 @@ def chdir_to_crate_root():
 
 def remove_existing_models():
     if os.path.isdir(MODELS_DIRECTORY):
+        print(f"Deleting existing {MODELS_DIRECTORY}")
         shutil.rmtree(MODELS_DIRECTORY)
 
 
 def run_sea_orm_cli():
+    print("Running sea-orm-cli generate entity")
     subprocess.check_call(
         [
             "sea-orm-cli",
