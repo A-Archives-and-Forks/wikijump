cc3605fa02a21afe832cf5b0734b6ba2dae8d507
Emmie Smith <emmie.maeda@gmail.com>
2026-04-19 13:48:01 -0400

    Add initial datetime fields insertion.

--- deepwell/scripts/generate-models.py
+++ deepwell/scripts/generate-models.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
+from glob import iglob
 import os
+from io import StringIO
 import re
-import subprocess
 import shutil
+import subprocess
 import sys
 
 DATABASE_URL = "postgres://wikijump:wikijump@localhost/wikijump"
 MODELS_DIRECTORY = "src/models"
+
+TIMESTAMP_ATTRIBUTE = "time::serde::rfc3339"
+OPTION_TIMESTAMP_ATTRIBUTE = "time::serde::rfc3339::option"
+TIMESTAMP_FIELD_REGEX = re.compile(r"( *)pub ([^:]+): TimeDateTimeWithTimeZone,\n")
+OPTION_TIMESTAMP_FIELD_REGEX = re.compile(
+    r"( *)pub ([^:]+): Option<TimeDateTimeWithTimeZone>,\n",
+)
 
 
 def chdir_to_crate_root():
@@ -46,12 +55,59 @@ def run_sea_orm_cli():
     )
 
 
-def add_datetime_format():
-    print("Adding time::serde::rfc3339 formatting to datetime fields")
-    # TODO
+def model_files():
+    for path in iglob(os.path.join(MODELS_DIRECTORY, "*.rs")):
+        filename = os.path.basename(path)
+        yield path, filename
 
 
-def apply_patches():
+def add_datetime_format(path, filename):
+    print(f"Checking {filename} for datetime fields")
+
+    with open(path) as file:
+        lines = file.readlines()
+        original_len = file.tell()
+
+    def find_regex_match(line):
+        PATTERNS = [
+            (TIMESTAMP_FIELD_REGEX, TIMESTAMP_ATTRIBUTE),
+            (OPTION_TIMESTAMP_FIELD_REGEX, OPTION_TIMESTAMP_ATTRIBUTE),
+        ]
+
+        for regex, attribute in PATTERNS:
+            match = regex.match(line)
+            if match is not None:
+                indent = match[1]
+                return indent, attribute
+
+        return None
+
+    buffer = StringIO()
+    for line in lines:
+        match find_regex_match(line):
+            case indent, attribute:
+                # Write #[serde] line, then field
+                buffer.write(f'{indent}#[serde(with = "{attribute}")]\n')
+                buffer.write(line)
+
+            case None:
+                # Write line as-is
+                buffer.write(line)
+
+    modified_len = buffer.tell()
+    if original_len != modified_len:
+        print(f"Writing serde attributes to datetime fields in {filename}")
+        with open(path, "w") as file:
+            file.write(buffer.getvalue())
+
+
+def apply_patches(model_path, filename):
+    patch_path = f"{model_path}.patch"
+    if not os.path.isfile(patch_path):
+        # No patch to process
+        return
+
+    print(f"Applying patch {patch_path}")
     # TODO
     # See https://github.com/SeaQL/sea-orm/issues/2358
 
@@ -60,5 +116,6 @@ if __name__ == "__main__":
     chdir_to_crate_root()
     remove_existing_models()
     run_sea_orm_cli()
-    add_datetime_format()
-    apply_patches()
+    for path, filename in model_files():
+        add_datetime_format(path, filename)
+        apply_patches(path, filename)
